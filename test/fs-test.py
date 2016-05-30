30061bc2615d31cbaceb0a8965a125fee7ff461e
Dani Hodovic <dani.hodovic@gmail.com>
2016-05-30 23:02:07 +0200

    Add test for directory above

--- test/fs-test.py
+++ test/fs-test.py
@@ -11,15 +11,17 @@ import main
 
 join = os.path.join
 
-class TestFileSystem(unittest.TestCase):
+class RelativeRequire(unittest.TestCase):
 
     def test_find_relative_same_dir(self):
         temp_dir = tempfile.mkdtemp(prefix='node-require-test')
         try:
             open(temp_dir + '/required.js', 'a').close()
             result = main.find_relative(temp_dir + '/main.js', './required')
+
             expected = temp_dir + '/required.js'
             self.assertEqual(expected, result)
+
         except Exception as err:
             self.fail(err)
             shutil.rmtree(temp_dir)
@@ -31,8 +33,24 @@ class TestFileSystem(unittest.TestCase):
             os.makedirs(nested_dir)
             open(nested_dir + '/required.js', 'a').close()
             result = main.find_relative(temp_dir + '/main.js', './foo/bar/baz/required')
+
             expected = temp_dir + '/foo/bar/baz/required.js'
             self.assertEqual(expected, result)
+
+        except Exception as err:
+            self.fail(err)
+            shutil.rmtree(temp_dir)
+
+    def test_find_relative_nested_dir_above(self):
+        temp_dir = tempfile.mkdtemp(prefix='node-require-test')
+        try:
+            open(temp_dir + '/required.js', 'a').close()
+            current_file = temp_dir + '/foo/main.js'
+            result = main.find_relative(current_file, '../required')
+
+            expected = temp_dir + '/required.js'
+            self.assertEqual(expected, result)
+
         except Exception as err:
             self.fail(err)
             shutil.rmtree(temp_dir)
